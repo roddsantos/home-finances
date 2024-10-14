a2826f54dbaed16c518607b037bf022dbbdad4d8
Rodrigo <rodri.soares95@gmail.com>
2024-10-14 11:20:09 -0400

    feat: logout in profile section

--- src/app/pages/settings/profile/profile.settings.ts
+++ src/app/pages/settings/profile/profile.settings.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, inject, Input, Output } from "@angular/core";
 import {
     FormControl,
     FormGroup,
@@ -7,10 +7,14 @@ import {
     ReactiveFormsModule,
     Validators,
 } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
 import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
-import { forkJoin, mergeMap, switchMap, zip } from "rxjs";
+import { debounceTime, distinctUntilChanged } from "rxjs";
 import { CardComponent } from "src/app/components/card/card.component";
+import { LocalStorageService } from "src/app/services/local-storage.service";
+import { ServiceUser } from "src/app/services/user.service";
 import { UserState } from "src/app/subjects/subjects.user";
 import { User } from "src/app/types/objects";
 import { ColorPipe } from "src/utils/pipes/colors";
@@ -29,18 +33,16 @@ import { ColorPipe } from "src/utils/pipes/colors";
         MatFormField,
         MatLabel,
         MatInputModule,
+        MatIconModule,
+        MatButtonModule,
     ],
 })
 export class ProfileSettingsComponent {
-    constructor() {
-        this.profileForm.valueChanges.subscribe((data) => {
-            this.setProfile.emit({ ...data });
-        });
-    }
-
     @Input() user: any;
     @Output() changed = new EventEmitter<boolean>();
     @Output() setProfile = new EventEmitter<Partial<User>>();
+    public storage = inject(LocalStorageService);
+    public userService = inject(ServiceUser);
 
     public profileForm = new FormGroup({
         name: new FormControl<string>("", {
@@ -61,9 +63,33 @@ export class ProfileSettingsComponent {
         name: "your name can't be empty",
         surname: "your surname can't be empty",
         username: "your surname can't be empty",
+        usernameExists: "this username already exists",
     };
+
+    constructor() {
+        const userStored = this.storage.getUser();
+        this.profileForm.valueChanges
+            .pipe(debounceTime(350), distinctUntilChanged())
+            .subscribe((data) => {
+                if (userStored?.username !== data.username) {
+                    this.userService.getUser(data.username || "").subscribe({
+                        next: (user) => {
+                            if (user)
+                                this.profileForm.controls["username"].setErrors({
+                                    usernameExists: true,
+                                });
+                        },
+                    });
+                }
+                this.setProfile.emit({ ...data });
+            });
+    }
 
     ngOnInit() {
         this.profileForm.patchValue({ ...this.user });
+    }
+
+    onLogout() {
+        this.storage.removeUser();
     }
 }
