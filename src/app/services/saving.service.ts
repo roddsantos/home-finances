dc20039668aeaf339d54a187b79504484da5662d
Rodrigo <rodrigo.santos@grupoicts.com.br>
2025-05-03 21:15:44 -0400

    feat: savings list in bank view

--- src/app/services/saving.service.ts
+++ src/app/services/saving.service.ts
@@ -3,6 +3,7 @@ import { Service } from "./service";
 import { mergeMap } from "rxjs";
 import { DatabaseTrackType, SavingsType } from "../core/types/objects";
 import { SAVINGS } from "src/utils/constants/services";
+import { FetchPaginatedData } from "../core/types/services";
 
 @Injectable({
     providedIn: "root",
@@ -10,5 +11,11 @@ import { SAVINGS } from "src/utils/constants/services";
 export class ServiceSaving extends Service {
     createSaving(data: Omit<SavingsType, keyof DatabaseTrackType>) {
         return this.httpClient.post<SavingsType>(SAVINGS, data);
+    }
+
+    getSavings(bankId: string, page: number) {
+        return this.httpClient.get<FetchPaginatedData<SavingsType>>(
+            SAVINGS + `/all?bankId=${bankId}&page=${page}`
+        );
     }
 }
