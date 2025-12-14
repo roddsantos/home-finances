a8e1b81abe4e180138da4710c989a6e181e35d01
Rodrigo <rodrigo.santos@grupoicts.com.br>
2025-12-14 18:58:10 -0400

    enh: removing commom components from templates

--- src/app/pages/bills/templates/bank/bank.template.bills.ts
+++ src/app/pages/bills/templates/bank/bank.template.bills.ts
@@ -1,18 +1,9 @@
-import { Dialog } from "@angular/cdk/dialog";
-import { CommonModule, CurrencyPipe, DatePipe } from "@angular/common";
+import { CommonModule } from "@angular/common";
 import { Component, inject, Input } from "@angular/core";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatIconModule } from "@angular/material/icon";
-import { ActionsComponent } from "src/app/components/actions/actions.component";
-import { CustomSnackbarComponent } from "src/app/components/custom-snackbar/custom-snackbar.component";
-import { ModalEditBill } from "src/app/components/modal/edit-bill/edit-bill.modal";
-import { ServiceBill } from "src/app/services/bill.service";
-import { BillState } from "src/app/core/subjects/subjects.bill";
-import { ActionItem } from "src/app/core/types/components";
 import { Bill, BillData } from "src/app/core/types/objects";
 import { GeneralState } from "src/app/core/subjects/subjects.general";
-import { ModalViewItem } from "src/app/components/modal/view-item/view-item.modal";
-import { GeneralService } from "src/app/services/general.service";
 import { MatTooltipModule } from "@angular/material/tooltip";
 
 @Component({
